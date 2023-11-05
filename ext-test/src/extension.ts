5d5476bd8d1dc1d288089eb8dab21fee90a6b15b
Cole0609 <cah062003@gmail.com>
2023-11-05 13:12:02 -0500

    added object reading from json

--- ext-test/src/extension.ts
+++ ext-test/src/extension.ts
@@ -1,16 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import db from "./database";
-import { EndPoint, ProblemStatus } from "leetcode-api-ts/dist/utils/interfaces";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	const questions = db.questions;
-	
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ext-test" is now active!');
@@ -19,9 +12,23 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand("ext-test.helloWorld", () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage("Hello World from ext_test! test");
+		vscode.window
+			.showInputBox({
+				prompt: "Enter the ID of the question you want to solve",
+				placeHolder: "Question ID",
+			})
+			.then((value) => {
+				if (value) {
+					// Handle the input value here
+					// You can use the value to fetch the corresponding question from the database
+					const question = db.questions.find((q) => q.id === Number(value));
+					if (question) {
+						vscode.window.showInformationMessage(question.title);
+					} else {
+						vscode.window.showErrorMessage("Question not found");
+					}
+				}
+			});
 	});
 
 	context.subscriptions.push(disposable);
