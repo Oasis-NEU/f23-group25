81c5066a2f910cc662cb4ef5cc0b77f3e15d0826
danielma4 <ma.daniel004@gmail.com>
2023-11-05 14:05:29 -0500

    added display features

--- ext-test/out/extension.js
+++ ext-test/out/extension.js
@@ -1,16 +1,51 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require("vscode");
-const leetcode_api_ts_1 = require("leetcode-api-ts");
-const interfaces_1 = require("leetcode-api-ts/dist/utils/interfaces");
+const vscode = __importStar(require("vscode"));
+const fs = __importStar(require("fs"));
+const database_1 = __importDefault(require("./database"));
+function displayLeetCodeQuestions() {
+    const filePath1 = 'f23-group25/ext-test/src/question.txt';
+    const filePath2 = 'f23-group25/ext-test/src/testCode.py';
+    const leetCodeQuestions = fs.readFileSync(filePath1, 'utf-8');
+    const pyCode = fs.readFileSync(filePath2, 'utf-8');
+    vscode.window.showInformationMessage('Here are sum questions!');
+    vscode.workspace.openTextDocument({ content: leetCodeQuestions }).then((doc) => {
+        vscode.window.showTextDocument(doc, vscode.ViewColumn.One, true);
+    });
+    vscode.workspace.openTextDocument({ language: 'python', content: pyCode }).then((doc) => {
+        vscode.window.showTextDocument(doc, vscode.ViewColumn.Two, true);
+    });
+}
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-async function activate(context) {
-    const leetcode = await leetcode_api_ts_1.default.build("oasisfall2023", "Password1.", interfaces_1.EndPoint.US // or EndPoint.CN
-    );
+function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "ext-test" is now active!');
@@ -18,11 +53,28 @@ async function activate(context) {
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand("ext-test.helloWorld", () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage("Hello World from ext_test! test");
+        vscode.window
+            .showInputBox({
+            prompt: "Enter the ID of the question you want to solve",
+            placeHolder: "Question ID",
+        })
+            .then((value) => {
+            if (value) {
+                // Handle the input value here
+                // You can use the value to fetch the corresponding question from the database
+                const question = database_1.default.questions.find((q) => q.id === Number(value));
+                if (question) {
+                    vscode.window.showInformationMessage(question.title);
+                }
+                else {
+                    vscode.window.showErrorMessage("Question not found");
+                }
+            }
+        });
     });
+    let display = vscode.commands.registerCommand('ext-test.displayLeetCodeQuestions', displayLeetCodeQuestions);
     context.subscriptions.push(disposable);
+    context.subscriptions.push(display);
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
